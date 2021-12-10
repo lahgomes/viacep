7f6506e1a2ff8791f56d90249f808a418b276c46
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-10 18:02:13 -0300

    layout cep

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 function App() {
   return (
 
-    <div className="container">
-      <label className="search">
-        <input className="search__input" type="number" placeholder="Digite o CEP"/>
-        <button className="search__button"><i className="fas fa-search-location"></i></button>
-      </label>
-    </div>
+    <main className="main">
+      <div className="container">
+        <label className="search">
+          <input className="search__input" type="text" placeholder="Digite o CEP"/>
+          <button className="search__button"><i className="fas fa-search-location"></i></button>
+        </label>
+      </div>
+    </main>
 
   );
 }
